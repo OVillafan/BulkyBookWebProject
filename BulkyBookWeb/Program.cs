d82556d1297186af9d81f8ed9e6ac1101f202204
OVillafan <ovillafan.ing@gmail.com>
2023-09-04 22:05:22 -0600

    View and Controller for create category page

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
